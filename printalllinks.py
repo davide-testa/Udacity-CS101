b4c512d436fab98694fd32266ad57c11e0134b0f
Davide Testa <48714242+davide-testa@users.noreply.github.com>
2019-03-24 21:29:33 +0100

    Update printalllinks.py

--- printalllinks.py
+++ printalllinks.py
@@ -1,3 +1,16 @@
+def get_page(url): 
+    try: 
+        import urllib 
+        return urllib.urlopen(url).read() 
+    except: 
+        return
+    
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+
 def print_all_links(page):
     while True:
         url, endpos = get_next_target(page)
