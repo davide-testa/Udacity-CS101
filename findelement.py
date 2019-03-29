abf1a98f430da8874e4290dc0ebe6908292bdd2e
Davide Testa <testadavide.td@gmail.com>
2019-03-29 15:32:01 +0100

    Create findelement.py

---
+++ findelement.py
@@ -0,0 +1,15 @@
+def find_element(p,t):
+    i = 0
+    while i < len(p):
+        if p[i] == t:
+            return i
+        i = i + 1
+    return
+
+def find_element(p,t):
+    i = 0
+    for e in p:
+        if e == t:
+            return i
+        i = i + 1
+    return -1
